523e87c5146da90da0d6b3bec30f886fca768469
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-25 12:18:24 +0500

    all the assets added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,9 @@ import type { AppProps } from "next/app";
 import Layout from "@/Layout/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return;
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
