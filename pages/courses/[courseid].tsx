b4f5481c51de063f191977d6c39a802aa42c7a76
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-27 20:31:52 +0500

    course detail page added

---
+++ pages/courses/[courseid].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Detail from "@/components/Home/LiveClassesAndMentorship/CourseDetail/CourseDetail";
+
+function courseId() {
+  return (
+    <>
+      <Detail />
+    </>
+  );
+}
+
+export default courseId;
