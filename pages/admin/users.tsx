1ddd2633ada4e776daefa75ac6be5f4ffd1b9b13
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-29 21:36:05 +0500

    user page structured

---
+++ pages/admin/users.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import User from "../../components/Admin/User/User";
+
+const index: React.FC = () => {
+  return (
+    <>
+      <User />
+    </>
+  );
+};
+
+export default index;
