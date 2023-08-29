f90898ea65c1f1e70ba948d3c09d374f5b01c889
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-29 21:34:04 +0500

    courses ui developed

---
+++ pages/admin/courses.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Courses from "../../components/Admin/Courses/Courses";
+
+const index: React.FC = () => {
+  return (
+    <>
+      <Courses />
+    </>
+  );
+};
+
+export default index;
