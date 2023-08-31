70d54a3795f37ddafa969b70decee20e0f8f8406
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-31 21:09:59 +0500

    add feedback page layout set

---
+++ pages/admin/addfeedback.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddFeedBack from "../../components/Admin/AddFeedBack/AddFeedBack";
+
+const index: React.FC = () => {
+  return (
+    <>
+      <AddFeedBack />
+    </>
+  );
+};
+
+export default index;
