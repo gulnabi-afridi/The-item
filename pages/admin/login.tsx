644e3cc57763f0fe259826156e2a9ad8fa961f3f
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2023-08-31 21:11:45 +0500

    login route set

---
+++ pages/admin/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../../components/Admin/Login/Login";
+
+const index: React.FC = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+
+export default index;
